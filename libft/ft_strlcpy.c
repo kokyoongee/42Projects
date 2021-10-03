10b561b01a9c270b9de7c047be6a5dbcbe85d03d
Kok Yoong <kokyoongee@gmail.com>
2021-10-03 14:45:28 +0800

    fix

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,23 +1,23 @@
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	src_len;
+	size_t	i;
 
-	if (src)
+	i = 0;
+	while (src[i])
+		i++;
+	if (dstsize == 0)
+		return (i);
+	i = 0;
+	while (src[i] && i < dstsize - 1)
 	{
-		src_len = ft_strlen(src);
-		if (size > 0)
-		{
-			if (src_len < size - 1)
-				ft_memcpy(dest, src, src_len + 1);
-			else
-			{
-				ft_memcpy(dest, src, size - 1);
-				dest[size - 1] = '\0';
-			}
-		}
-		return (src_len);
+		dst[i] = src[i];
+		i++;
 	}
-	return (0);
+	dst[i] = '\0';
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
 }
