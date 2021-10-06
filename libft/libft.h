600d34bdc98b43a7f18fc44edd70bf14da978f20
Kok Yoong <kokyoongee@gmail.com>
2021-10-06 12:42:24 +0800

    fix

--- libft/libft.h
+++ libft/libft.h
@@ -18,7 +18,6 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
-char	*ft_strcat(char *s1, const char *s2);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
